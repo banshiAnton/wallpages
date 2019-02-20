cc10b32e8dd3ae2e51ee80bc9b24f56f1682a603
banshiAnton <banshi.anton@gmail.com>
2019-02-20 18:12:37 +0200

    timout test

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import {
-  tap, switchMap
+  tap, switchMap, timeout, catchError
 } from 'rxjs/operators';
+
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,25 +19,31 @@ export class ServiceService {
 
   postImages(data) {
     console.log('service', data);
-    return this.http.post(`${this.apiImageUrl}/upload`, data);
+    return this.http.post(`${this.apiImageUrl}/upload`, data).pipe(
+      timeout(5 * 1000), catchError(error => of({success: false, error}))
+    );
   }
 
   addCategory(name, tags) {
     console.log('Service', name, tags);
     return this.http.post(`${this.apiImageUrl}/add/category`, JSON.stringify({name, tags}), {
       headers: new HttpHeaders({ 'Content-Type': 'application/json'})
-    });
+    }).pipe(
+      catchError(error => of({success: false, error}))
+    );
   }
 
   setup() {
     return this.http.get(`${this.authUrl}/makeSetup`, {
       headers: new HttpHeaders({ 'Content-Type': 'application/json'})
-    });
+    }).pipe(
+      catchError(error => of({success: false, error}))
+    );
   }
 
   getCategories() {
     return this.http.get(`${this.apiImageUrl}/categories`).pipe(
-      tap(item => console.log('Resp', item))
+      catchError(error => of({success: false, error}))
     );
   }
 
@@ -44,49 +52,70 @@ export class ServiceService {
     JSON.stringify({name: category.name, tags: category.tags.map(tag => tag.value)}),
     {
       headers: new HttpHeaders({ 'Content-Type': 'application/json'})
-    });
+    }).pipe(
+      catchError(error => of({success: false, error}))
+    );
   }
 
   deleteCategory(id) {
     return this.http.delete(`${this.apiImageUrl}/category/${id}`, {
       headers: new HttpHeaders({ 'Content-Type': 'application/json'})
-    });
+    }).pipe(
+      catchError(error => of({success: false, error}))
+    );
   }
 
   addAdmin(vkid) {
     return this.http.post(`${this.authUrl}admin`, JSON.stringify({vkid}), {
       headers: new HttpHeaders({ 'Content-Type': 'application/json'})
-    });
+    }).pipe(
+      catchError(error => of({success: false, error}))
+    );
   }
 
   getAdmin(isMainAdmin = false) {
-    return this.http.get(`${this.authUrl}${isMainAdmin ? 'isMainAdmin' : 'admin'}`);
+    return this.http.get(`${this.authUrl}${isMainAdmin ? 'isMainAdmin' : 'admin'}`).pipe(
+      catchError(error => of({success: false, error}))
+    );
   }
 
   getImages(query = {count: 10, offset: 0, category: null}) {
     console.log(query);
     console.log(`${this.apiImageUrl}?${query.category ? 'category=' + query.category : ''}&count=${query.count}&offset=${query.offset}`);
-    return this.http.get(`${this.apiImageUrl}?${query.category ? 'category='+query.category : ''}&count=${query.count}&offset=${query.offset}`);
+    return this.http.get(`${this.apiImageUrl}?${query.category ? 'category=' +query.category : ''}&count=${query.count}&offset=${query.offset}`)
+    .pipe(
+      catchError(error => of({success: false, error}))
+    );
   }
 
   getAuthLinks() {
-    return this.http.get(`${this.authUrl}authLinks`);
+    return this.http.get(`${this.authUrl}authLinks`).pipe(
+      catchError(error => of({success: false, error}))
+    );
   }
 
   getAdmins() {
-    return this.http.get(`${this.authUrl}admins`);
+    return this.http.get(`${this.authUrl}admins`).pipe(
+      catchError(error => of({success: false, error}))
+    );
   }
 
   deleteAdmin(id: number) {
-    return this.http.delete(`${this.authUrl}admin/${id}`);
+    return this.http.delete(`${this.authUrl}admin/${id}`).pipe(
+      catchError(error => of({success: false, error}))
+    );
   }
 
   isInit() {
-    return this.http.get(`${this.apiImageUrl}/isInit`);
+    return this.http.get(`${this.apiImageUrl}/isInit`).pipe(
+      catchError(error => of({success: false, error}))
+    );
   }
 
   getLastTokenUpd() {
-    return this.http.get(`${this.apiImageUrl}/lastTokenUpd`);
+    return this.http.get(`${this.apiImageUrl}/lastTokenUpd`).pipe(
+      catchError(error => of({success: false, error}))
+    );
   }
 
 }
